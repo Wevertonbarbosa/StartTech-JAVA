4ed5d9acb76b1c976a73dc4741b38a089e767dbe
Weverton <wevertonlbs00@gmail.com>
2022-11-30 16:33:36 -0300

    dedsafio 2, falta fazer a lista nova

--- Lacos-repeticoes/src/Desafio2.java
+++ Lacos-repeticoes/src/Desafio2.java
@@ -8,21 +8,27 @@ public class Desafio2 {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         int total = 0;
+
         System.out.println("Quantas rodadas você deseja, digite um número: ");
         int number = scanner.nextInt();
 
         System.out.println("Quantidades de dados no grupo: ");
         int dados = scanner.nextInt();
-        ArrayList<Integer> bankDate = new ArrayList<Integer>();
-        bankDate.add(random.nextInt(1,10));
 
-        //Falta terminar o Desafio, Problema(Acertar a parte dos números do Random de uma forma Dinâmica)
-        for (int c = 1; c <= number; c++) {
+
+
+        for (int x = 0; x < number; x++) {
 
             for (int j = 0; j < dados; j++) {
-                System.out.println(bankDate.get(j));
+                int sorteio = random.nextInt(1,7);
+                System.out.print(sorteio);
+                total += sorteio;
 
+                if (j < dados - 1 ){
+                    System.out.print(" + ");
+                }
             }
+            System.out.println(" = " + total);
 
         }
 
