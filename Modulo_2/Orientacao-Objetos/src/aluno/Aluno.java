b96976b33773a4a38797f2d5eff66326604f2171
wevertonbarbosa <wevertonlbs00@gmail.com>
2022-12-26 19:45:46 -0300

    ajustar o setNotas, não estou conseguindo direcionar o indece do Array

--- Modulo_2/Orientacao-Objetos/src/aluno/Aluno.java
+++ Modulo_2/Orientacao-Objetos/src/aluno/Aluno.java
@@ -2,10 +2,24 @@ package aluno;
 
 public class Aluno {
 
-   public String nomes;
-   public final double[] notas = new double[3];
+   private String nomes;
+    public String getNomes() {
+        return nomes;
+    }
+    public void setNomes(String nomes) {
+        this.nomes = nomes;
+    }
 
-   double calculandoMedia() {
+
+    private double[] notas = new double[3];
+
+    public void setNotas(double[] notas) {
+        this.notas = notas;
+    }
+
+
+
+    double calculandoMedia() {
        double media = 0;
        for (double soma : notas) {
            media += soma;
--- Modulo_2/Orientacao-Objetos/src/aluno/Alunos_Objetos.java
+++ Modulo_2/Orientacao-Objetos/src/aluno/Alunos_Objetos.java
@@ -11,15 +11,15 @@ public class Alunos_Objetos {
 
         for (int i = 0; i < alunos.length; i++) {
             Aluno aluno = new Aluno();
-            aluno.nomes = leitor.lerString("Nome do Aluno: " + (i + 1));
+            aluno.setNomes(leitor.lerString("Nome do Aluno: " + (i + 1)));
 
-            aluno.notas[0] = leitor.lerDecimal("Digite a nota 1 : " );
-            aluno.notas[1] = leitor.lerDecimal("Digite a nota 2 : " );
-            aluno.notas[2] = leitor.lerDecimal("Digite a nota 3 : " );
+            aluno.setNotas(new double[0]);{leitor.lerDecimal("Digite a nota 1: ");};
+            aluno.setNotas(new double[1]);{leitor.lerDecimal("Digite a nota 2: ");};
+            aluno.setNotas(new double[2]);{leitor.lerDecimal("Digite a nota 3: ");};
             alunos[i] = aluno;//Armazena os valores.
         }
         for (Aluno aluno : alunos) {
-            System.out.printf("O aluno, %s teve a média: %.2f \n", aluno.nomes, aluno.calculandoMedia());
+            System.out.printf("O aluno, %s teve a média: %.2f \n", aluno.getNomes(), aluno.calculandoMedia());
         }
 
 
