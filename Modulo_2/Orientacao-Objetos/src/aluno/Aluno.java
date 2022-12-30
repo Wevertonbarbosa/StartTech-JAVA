87ffe709a32fffb966b7d4ffda86e5234afb9e45
wevertonbarbosa <wevertonlbs00@gmail.com>
2022-12-29 22:54:55 -0300

    terminando o exercicio Aluno, falta concluir o exercicio baralho

--- Modulo_2/Orientacao-Objetos/src/aluno/Aluno.java
+++ Modulo_2/Orientacao-Objetos/src/aluno/Aluno.java
@@ -4,8 +4,16 @@ import utils.Leitor;
 
 public class Aluno {
     private String nomes;
-    private double[] notas = new double[3];
-    private int qtaAlunos = 3;
+
+    public String getNomes() {
+        return nomes;
+    }
+
+    public void setNomes(String nomes) {
+        this.nomes = nomes;
+    }
+
+    double[] notas = new double[3];
 
     public Aluno() {
         this.nomes = nomes;
@@ -15,13 +23,13 @@ public class Aluno {
 
 
 
-//    double calculandoMedia() {
-//        double media = 0;
-//        for (double soma : notas) {
-//            media += soma;
-//        }
-//        return media / notas.length;
-//    }
+    double calculandoMedia() {
+        double media = 0;
+        for (double soma : notas) {
+            media += soma;
+        }
+        return media / notas.length;
+    }
 
 
 }
